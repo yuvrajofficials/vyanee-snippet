2e71501342a7c30217848ccfca6e40410b746499
yuvrajofficials <your_email@example.com>
2025-08-06 13:14:13 +0530

    Minor fixes in the button component

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/__docs__/*.stories.tsx",
+    "../src/**/__docs__/*.mdx"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
